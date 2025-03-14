4ccfe6e9552b51b8e90bb9a882fb19b4fcccaadb
Kayxue <s0990006@gmail.com>
2025-03-14 20:26:33 +0800

    Change parser

--- lib/src/parser/signature.rs
+++ lib/src/parser/signature.rs
@@ -31,12 +31,12 @@ use sha2::Sha256;
 ///     // NG
 /// }
 /// ```
-pub fn validate_signature(channel_secret: &str, signature: &str, body: &[u8]) -> bool {
+pub fn validate_signature(channel_secret: &str, signature: &str, body: &str) -> bool {
     type HmacSha256 = Hmac<Sha256>;
 
     let mut mac = HmacSha256::new_from_slice(channel_secret.as_bytes())
         .expect("HMAC can take key of any size");
-    mac.update(body);
+    mac.update(body.as_bytes());
     let mut buf = String::new();
     general_purpose::STANDARD.encode_string(mac.finalize().into_bytes(), &mut buf);
     buf == signature
